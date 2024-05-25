93a3342c8e03139896470d61af7d123bcccf28a3
chucaihuang <chucaihuang@kugou.net>
2024-05-25 18:01:55 +0800

    feat: 图片上传的功能处理

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Toaster } from '@/components/ui/toaster'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,6 +28,7 @@ export default function RootLayout({
           <Footer/>
         </div>
       </main>
+      <Toaster />
       </body>
       </html>
   );
