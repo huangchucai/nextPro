610ba9ae2130f25c94a2e5d137b539de653d4af6
黄楚才 <chucaihuang@kugou.net>
2024-05-27 20:50:29 +0800

    feat：步骤的开发

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,11 @@
 import React, { ReactNode } from 'react';
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import Steps from '@/components/Steps'
 
 function Layout({ children }: { children: ReactNode }) {
   return (
       <MaxWidthWrapper className={'flex-1 flex flex-col'}>
+        <Steps />
         {children}
       </MaxWidthWrapper>
   );
